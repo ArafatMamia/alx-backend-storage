36abddf83a0f848e263178fc1b84e6f431e76211
Arafat <arafmamia@gmail.com>
2022-06-29 07:25:41 -0700

    unig

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
---sql script that create a table user
-create TABLE IF NOT EXISTS users( 
-  id int not NULL AUTOINCREMENT 
-  email varchar(255), not NULL UNIQUE,
-  name varchar(255),
-  PRIMARY KEY(id)
-  );
+-- Task : 0. We are all unique! - creates a table users
+-- script can be executed on any database
+CREATE TABLE If NOT EXISTS `users` (  
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255)
+);
