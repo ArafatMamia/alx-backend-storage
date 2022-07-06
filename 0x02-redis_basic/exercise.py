addefaf219945189067cc1cb00b7b5f987993df8
Arafat Mamia <arafmamia@gmail.com>
2022-07-06 06:24:02 -0700

    main

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Tasks - Redis """
+
+import redis
+from typing import Union
+from uuid import uuid4, UUID
+
+class Cache:
+    """ Class for implementing a Cache """
+
+    def __init__(self):
+        """ Constructor Method """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store the input data in Redis using a
+        random key and return the key.
+        """
+        random_key = str(uuid4())
+        self._redis.set(random_key, data)
+        
+        return random_key
